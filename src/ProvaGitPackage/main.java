6054d1b42f7345771795d4e4c9e73477680e29cb
Mirko_Pardo <56472010+Arcadal@users.noreply.github.com>
2023-06-07 11:40:59 +0200

    Update github

--- src/ProvaGitPackage/main.java
+++ src/ProvaGitPackage/main.java
@@ -6,6 +6,7 @@ public class main {
 	
 		System.out.println("Avvio Main");
 		System.out.println("Esecuzione del programma");
+		System.out.println("Fase 1");
 		System.out.println("Fine Programma");
 		
 	}
