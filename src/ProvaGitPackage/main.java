f28cc9dbe0c7b2903ab33c2cf80f501400881820
Mirko_Pardo <mirkopardo267@gmail.com>
2023-06-07 10:54:40 +0200

    Modifica main 2

--- src/ProvaGitPackage/main.java
+++ src/ProvaGitPackage/main.java
@@ -5,6 +5,9 @@ public class main {
 	public static void main(String[] args) {
 	
 		System.out.println("Avvio Main");
+		System.out.println("Esecuzione del programma");
+		System.out.println("Fine Programma");
+		
 	}
 
 }
